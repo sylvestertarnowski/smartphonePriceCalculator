6a2be564c939d10aa6c2cd32de18bd9f9ded8177
sylvestertarnowski <34459277+sylvestertarnowski@users.noreply.github.com>
2018-01-06 20:14:28 +0100

    Add files via upload

---
+++ smartphonePriceCalculator/script.js
@@ -0,0 +1,10 @@
+/* Writing a program to calculate the total price of your phone purchase.
+Keep purchasing until you run out of money in your bank accountr.
+Buy accessories for each phone as long as your purchase is below your mental
+spending threshold.
+
+After the purchase amount is calculated, add in the tax, then print out
+the calculated purchase amount, properly formatted.
+
+Finally, check the amount against your bank account balance to see if
+you can afford it or not. */
