f7a1ba896666544c5d2f7804f1444ff59779f39d
sylvestertarnowski <34459277+sylvestertarnowski@users.noreply.github.com>
2018-01-06 20:40:45 +0100

    Add files via upload
    added css stylesheet, and some basic styling

--- smartphonePriceCalculator/script.js
+++ smartphonePriceCalculator/script.js
@@ -8,3 +8,10 @@ the calculated purchase amount, properly formatted.
 
 Finally, check the amount against your bank account balance to see if
 you can afford it or not. */
+
+var phonePrice = prompt( 'Please tell me how much does the phone cost?');
+var taxRate = 0.23;
+var phoneCase = prompt('How much does the phone case cost?');
+var phoneHeadphones = prompt('How much for the headphones?');
+var phoneGlass = prompt('How much does the tempered glass cost?');
+var mentalSpendingLimit = phonePrice/3;
